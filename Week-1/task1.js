b0768d223af2d5a6952649993b9d10373c0617ad
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-07-23 20:41:56 +0700

    Task 1 - minggu pertama

---
+++ Week-1/task1.js
@@ -0,0 +1,39 @@
+const biodata = {
+    name: "Galuh Prahadi Gumelar",
+    age: 24,
+    hobbies: ["Memancing", "Traveling", "Desain", "Coding"],
+    isMarried: false,
+    schoolList: [{
+        name: "SDN 2 Gondang",
+        yearIn: "2002",
+        yearOut: "2008",
+        major: null
+    }, {
+        name: "SMPN 2 TUGU",
+        yearIn: "2008",
+        yearOut: "2011",
+        major: null
+    }, {
+        name: "SMAN 1 TUGU",
+        yearIn: "2011",
+        yearOut: "2014",
+        major: "IPS"
+    }, {
+        name: "UNIVERSITAS NUSANTARA PGRI KEDIRI",
+        yearIn: "2014",
+        yearOut: "2018",
+        major: "SISTEM INFORMASI"
+    }],
+    skills: [
+        {
+            skillName: "PHP",
+            level: "advanced"
+        }, {
+            skillName: "JAVASCRIPT",
+            level: "beginner"
+        }
+    ],
+    interestInCoding: true
+};
+
+//console.log(biodata);

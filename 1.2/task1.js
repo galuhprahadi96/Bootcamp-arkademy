7f93681583e93a3f9cd379862ee77e182b57b481
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-07-27 20:59:36 +0700

    task1 - fungsi build-in javascript

---
+++ 1.2/task1.js
@@ -0,0 +1,16 @@
+const arr = [8, 2, 9, 1, 3, 5, 7, 6, 4];
+
+// 1. Sort = mengurutkan data array
+const sortData = arr.sort();
+// console.log(sortData);
+
+
+// 2. Filter = menyaring data array
+const filterData = arr.filter(val => {
+    return val <= 5
+})
+// console.log(filterData);
+
+// 4. Push = menambah data ke dalam array
+arr.push("Galuh", "Prahadi", "Gumelar");
+// console.log(arr);

ed3caa02f8b84c444910155fad1ab4cff42bc2e5
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-07-29 20:08:33 +0700

    task 3 - fetch data API

---
+++ 1.3/task3.js
@@ -0,0 +1,7 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((response) => {
+    response.forEach((e) => {
+      console.log(e.name);
+    });
+  });

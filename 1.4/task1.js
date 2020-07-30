81271a5aea1dba50d799f94d98e9419b03010880
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-07-30 18:53:52 +0700

    pemberian note penjelasan pada deteksi palindrome

--- 1.4/task1.js
+++ 1.4/task1.js
@@ -1,9 +1,13 @@
 function reverseWord(word) {
+  // tampung data yang sudah di reverse
   let reverse = "";
 
+  // proses reverse data
   for (let x = word.length - 1; x >= 0; x--) {
     reverse += word[x];
   }
+
+  // pencocokan data
   if (word.toLowerCase() == reverse.toLowerCase()) {
     console.log("Palindrom");
   } else {

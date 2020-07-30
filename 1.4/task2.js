6965a7db5f44941b8eb4c2ebc73bca9451edffaf
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-07-30 19:10:14 +0700

    pembuatan fungsi biaya order

--- 1.4/task2.js
+++ 1.4/task2.js
@@ -2,18 +2,36 @@ function arkFood(harga, voucher, jarak, pajak) {
   console.log(`Harga : ${harga}`);
   diskon(voucher, harga);
   resto(harga, pajak);
+  biayaOrder(jarak);
 }
 
-function resto(harga, pajak) {
-  let dikenakan = "";
-  if (pajak === true) {
-    dikenakan = (harga * 5) / 100;
+// hitung biayaOrder
+function biayaOrder(jarak) {
+  let biaya;
+  if (jarak == 2) {
+    biaya = 5000;
+    console.log(biaya);
+  } else if (jarak > 2) {
+    biaya = (jarak - 2) * 3000 + 5000;
+    console.log(biaya);
   } else {
-    dikenakan = 0;
+    biaya = 3000;
+    console.log(biaya);
   }
-  console.log(dikenakan);
 }
 
+// pajakResto
+function pajakResto(harga, pajak) {
+  let pjResto = "";
+  if (pajak === true) {
+    pjResto = (harga * 5) / 100;
+  } else {
+    pjResto = 0;
+  }
+  console.log(pjResto);
+}
+
+// diskon
 function diskon(voucher, harga) {
   // diskon
   let diskon = "";
@@ -27,4 +45,4 @@ function diskon(voucher, harga) {
   console.log(diskon);
 }
 
-arkFood(75000, "ARKAFOOD5", 5, true);
+arkFood(75000, "ARKAFOOD5", 10, true);
